3a8d7635c39412a9ac58e92d8bf9e5d2138132e3
Eryson Miranda <eryson.ktm@gmail.com>
2020-08-11 17:21:53 -0300

    fix: app test should respond at the root

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,9 @@ import UsersController from "./controllers/UsersController";
 
 const routes = new Router();
 
+routes.get("/", function (req, res) {
+  res.send("It's Running!!!");
+});
 routes.get("/users", UsersController.index);
 
 export default routes;
