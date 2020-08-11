07ace4daeed697bca3aee7dfbf53c5cd188847a8
Eryson Miranda <eryson.ktm@gmail.com>
2020-08-11 17:29:33 -0300

    feat: created the users POST route

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -2,7 +2,16 @@ class UsersController {
   async index(req, res) {
     try {
       const users = [{ name: "Jhon Doe", mail: "jhon@mail.com" }];
-      return res.json(users);
+      return res.status(200).json(users);
+    } catch (error) {
+      return res.json(error);
+    }
+  }
+
+  async create(req, res) {
+    try {
+      const users = req.body;
+      return res.status(201).json(users);
     } catch (error) {
       return res.json(error);
     }
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ const routes = new Router();
 routes.get("/", function (req, res) {
   res.send("It's Running!!!");
 });
+
 routes.get("/users", UsersController.index);
+routes.post("/users", UsersController.create);
 
 export default routes;
