d082896fe5dd6da29e3c6c468e243c33f7570602
Eryson Miranda <eryson.ktm@gmail.com>
2020-11-12 14:51:35 -0300

    feat: Adds the username field in the users seed

--- src/database/seeds/001_users.js
+++ src/database/seeds/001_users.js
@@ -6,8 +6,12 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex("users").insert([
-        { name: "Eryson", email: "eryson@mail.com", password: "123" },
-        { name: "Ana", email: "ana@mail.com", password: "456" },
+        {
+          username: "admin",
+          name: "Admin",
+          email: "admin@mail.com",
+          password: "12345",
+        },
       ]);
     });
 };
