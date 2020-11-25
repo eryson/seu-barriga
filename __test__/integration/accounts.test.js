18e90e27ba2f73715a497ef1824af72680512105
eryson <eryson.ktm@gmail.com>
2020-11-25 10:42:25 -0300

    feat: Create should list only user accounts test

--- __test__/integration/accounts.test.js
+++ __test__/integration/accounts.test.js
@@ -2,24 +2,38 @@ import request from "supertest";
 import app from "../../src/app";
 import generateToken from "../../src/utils/generateToken";
 
-const testToken = generateToken();
+const token = generateToken();
 
 describe("Accounts Tests", () => {
   let user;
   let account;
+  let secondaryUser;
+  let secondaryAccount;
 
   beforeAll(async (done) => {
-    const res = await request(app)
+    const response = await request(app)
       .post("/users")
       .send({
-        username: "darkside",
-        name: "Darth Vader",
-        email: "darkside@theForce.com",
-        password: "Empire",
+        username: "user_test",
+        name: "User Test",
+        email: "user_test@mail.com",
+        password: "userTest",
       })
-      .set("Authorization", `Bearer ${testToken}`);
+      .set("Authorization", `Bearer ${token}`);
 
-    user = res;
+    user = response;
+
+    const responseSecondaryUser = await request(app)
+      .post("/users")
+      .send({
+        username: "secondary_user",
+        name: "Secondary User",
+        email: "secondary_user@mail.com",
+        password: "secondaryUser",
+      })
+      .set("Authorization", `Bearer ${token}`);
+
+    secondaryUser = responseSecondaryUser;
     done();
   });
 
@@ -27,7 +41,7 @@ describe("Accounts Tests", () => {
     const res = await request(app)
       .post("/accounts")
       .send({ name: "NuBank #1", user: user.body[0] })
-      .set("Authorization", `Bearer ${testToken}`);
+      .set("Authorization", `Bearer ${token}`);
 
     account = res;
     expect(res.status).toBe(201);
@@ -37,7 +51,7 @@ describe("Accounts Tests", () => {
   it("Should return all accounts", async (done) => {
     const res = await request(app)
       .get("/accounts")
-      .set("Authorization", `Bearer ${testToken}`);
+      .set("Authorization", `Bearer ${token}`);
 
     expect(res.status).toBe(200);
     expect(res.body.length).toBeGreaterThan(0);
@@ -47,7 +61,7 @@ describe("Accounts Tests", () => {
   it("Should return an account by id", async (done) => {
     const res = await request(app)
       .get(`/accounts/${account.body[0]}`)
-      .set("Authorization", `Bearer ${testToken}`);
+      .set("Authorization", `Bearer ${token}`);
 
     expect(res.status).toBe(200);
     expect(res.body[0].name).toBe("NuBank #1");
@@ -59,24 +73,56 @@ describe("Accounts Tests", () => {
     const res = await request(app)
       .put(`/accounts/${account.body[0]}`)
       .send({ name: "Inter #1" })
-      .set("Authorization", `Bearer ${testToken}`);
+      .set("Authorization", `Bearer ${token}`);
 
     expect(res.status).toBe(200);
     done();
   });
 
-  it("Should delete an account by id", async (done) => {
+  it("Should list only user accounts", async (done) => {
     const res = await request(app)
-      .delete(`/accounts/${account.body[0]}`)
-      .set("Authorization", `Bearer ${testToken}`);
+      .post("/accounts")
+      .send({ name: "NuBank #1", user: secondaryUser.body[0] })
+      .set("Authorization", `Bearer ${token}`);
 
-    expect(res.status).toBe(204);
+    secondaryAccount = res;
+    expect(res.status).toBe(201);
 
     const response = await request(app)
-      .delete(`/users/${user.body[0].id}`)
-      .set("Authorization", `Bearer ${testToken}`);
+      .get(`/accounts/${secondaryAccount.body[0]}`)
+      .set("Authorization", `Bearer ${token}`);
 
-    expect(response.status).toBe(204);
+    expect(response.status).toBe(200);
+    expect(response.body[0].name).toBe("NuBank #1");
+    expect(response.body.length).toBe(1);
+
+    done();
+  });
+
+  it("Should delete an account by id", async (done) => {
+    const responseAccount = await request(app)
+      .delete(`/accounts/${account.body[0]}`)
+      .set("Authorization", `Bearer ${token}`);
+
+    expect(responseAccount.status).toBe(204);
+
+    const responseSecondaryAccount = await request(app)
+      .delete(`/accounts/${secondaryAccount.body[0]}`)
+      .set("Authorization", `Bearer ${token}`);
+
+    expect(responseSecondaryAccount.status).toBe(204);
+
+    const responseUser = await request(app)
+      .delete(`/users/${user.body[0].id}`)
+      .set("Authorization", `Bearer ${token}`);
+
+    expect(responseUser.status).toBe(204);
+
+    const responseSecondaryUser = await request(app)
+      .delete(`/users/${secondaryUser.body[0].id}`)
+      .set("Authorization", `Bearer ${token}`);
+
+    expect(responseSecondaryUser.status).toBe(204);
 
     done();
   });
--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 require("dotenv").config();
 
 // eslint-disable-next-line no-undef
